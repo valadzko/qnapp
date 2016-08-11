a1276c855609c1c87f50da366b75038e8106b62f
valadzko <pavel.valadzko@gmail.com>
2016-08-11 21:06:36 +0300

    Add test for question destroy option. Change migration to belongs_to

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Answer, type: :model do
 
   it { should validate_presence_of :body }
   it { should belong_to :question }
+  it { should belong_to(:question).dependent(:destroy) }
   
 end
