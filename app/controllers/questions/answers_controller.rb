2612f499ec3663ead5dec601462ba30d83cbe74e
valadzko <pavel.valadzko@gmail.com>
2016-08-17 22:49:25 +0300

    Latest changes - 2

--- app/controllers/questions/answers_controller.rb
+++ app/controllers/questions/answers_controller.rb
@@ -1,5 +1,14 @@
 class Questions::AnswersController < ApplicationController
+  before_action :find_question, only: [:new, :create]
 
-  def index
+  def new
+    @answer = @question.answers.new
   end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
+
 end
--- spec/controllers/questions/answers_controller_spec.rb
+++ spec/controllers/questions/answers_controller_spec.rb
@@ -3,18 +3,10 @@ require 'rails_helper'
 RSpec.describe Questions::AnswersController, type: :controller do
   let(:question) { create(:question) }
 
-  describe 'GET #index with :question_id' do
-      it 'populates an array of all answers for the question' do
-      #a = create(:answer, question: question)
-      #puts "YOUR ANSWER IS : #{a.inspect}"
-
-        puts "Your question is : #{question.inspect}"
-        question
-        a1 = create(:answer, question: question)
-        a2 = create(:answer, question: question)
-
-        get :index, question_id: question.id
-        expect(assigns(:answers)).to match_array[a1,a2]
+  describe 'GET #new answer' do
+      it 'create a new answer ' do
+        get :new, { question_id: question.id }
+        expect(assigns(:answer)).to be_a_new(Answer)
       end
 
   end
