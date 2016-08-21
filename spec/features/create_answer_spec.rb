8a0ab1255d766d9921d1277c7cac9963ae0faa5b
valadzko <pavel.valadzko@gmail.com>
2016-08-21 22:27:38 +0300

    Acceptance test for create answer spec

--- spec/features/create_answer_spec.rb
+++ spec/features/create_answer_spec.rb
@@ -5,9 +5,17 @@ feature 'Create Answer on question', %q{
   As authenticated user
   I want to be able to create answer on the question
 }do
-
-  scenario 'Authenticated user can create answer on the question' do
-    
+  given(:user) { create(:user) }
+  given(:question) { create(:question) }
+  scenario 'Authenticated user can create answer for the question' do
+    sign_in(user)
+    visit question_path(question)
+    click_on 'Add new answer'
+    fill_in 'Body', with: 'The answer text which is worth to type'
+    click_on 'Post Your Answer'
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+    expect(page).to have_content 'The answer text which is worth to type'
   end
 
   scenario 'Non-authenticated user can not create answer on the question'do
