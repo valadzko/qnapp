f4296402e55f91f9b219fe62733ffdc6eff1685e
valadzko <pavel.valadzko@gmail.com>
2016-08-25 11:35:42 +0300

    Fix problem with capybara methods

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,7 +39,7 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
-  config.include FeatureHelper
+  config.include FeatureHelper, type: :feature
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
