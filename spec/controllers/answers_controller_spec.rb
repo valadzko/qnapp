70394a4528efd625426d4f29586e91a4fbd0b7ab
valadzko <pavel.valadzko@gmail.com>
2016-09-03 17:08:52 +0300

    Create answer partial. Some more work on tests

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe AnswersController, type: :controller do
 
     context 'with valid attributes' do
       it 'saves new answer to database' do
-        expect { post :create, answer: attributes_for(:answer), question_id: question, format: :js }.to change(Answer, :count).by(1)
         expect { post :create, answer: attributes_for(:answer), question_id: question, format: :js }.to change { question.answers.count }.by(1)
       end
 
--- spec/features/create_answer_spec.rb
+++ spec/features/create_answer_spec.rb
@@ -17,6 +17,15 @@ feature 'Create Answer on question', %q{
     end
   end
 
+  scenario 'Authenticated user submit empty answer for the question', js: true do
+    sign_in create(:user)
+    visit question_path(question)
+    fill_in 'answer_body', with: '' # just to make sure it is empty
+    click_on 'Post Your Answer'
+    expect_page_to_have_question(question)
+    expect(question.answers.count).to eq 0
+  end
+
   scenario 'Non-authenticated user can not create answer on the question'do
     visit question_path(question)
     expect(page).to_not have_link 'Post Your Answer'
