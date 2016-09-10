5fc0f9a42b5c6d1d753f67a3861b815b94fe3f2c
valadzko <pavel.valadzko@gmail.com>
2016-09-10 16:40:17 +0300

    User can edit his question story w/o ajax

---
+++ spec/features/edit_question_spec.rb
@@ -0,0 +1,42 @@
+require_relative 'features_helper'
+
+feature 'Edit question', %q{
+  In order to correct mistake in question
+  As author of the question
+  I want to be able to edit my question
+} do
+
+  given(:author){ create(:user) }
+  given(:question) {create(:question, user: author)}
+  scenario 'Author can edit his question' do
+    sign_in(author)
+    visit question_path(question)
+    click_on 'Edit' # same as expect(page).to have_link 'Edit'
+    edited_title = question.title + 'title_appendix'
+    edited_body = question.body + 'body_appendix'
+    fill_in 'question_title', with: edited_title
+    fill_in 'question_body', with: edited_body
+    click_on 'Update Question'
+# TODO:
+#    within ('.question') do
+    expect(page).to have_content edited_title
+    expect(page).to have_content edited_body
+#    end
+  end
+
+  scenario 'Authenticated non-author can not edit the question' do
+    non_author = create(:user)
+    sign_in(non_author)
+    visit question_path(question)
+    expect(page).to_not have_link 'Edit' # how to apply only to question?
+    # how to protect us from direct get request? should we have this test as feature?
+  end
+
+  scenario 'Non-authenticated user can not edit the question' do
+    visit question_path(question)
+    expect(page).to_not have_link 'Edit'
+  end
+
+
+
+end
