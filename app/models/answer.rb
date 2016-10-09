cbd95152f27e84be60cc72c07edc20eea9e659d0
valadzko <pavel.valadzko@gmail.com>
2016-10-09 08:45:49 +0300

    Changes for accepted answer button image

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,8 +4,14 @@ class Answer < ApplicationRecord
 
   validates :body,:user_id, presence: true
 
+  default_scope { order(accepted: :desc) }
+  
   def mark_as_accepted
     question.answers.update_all(accepted:false)
     update(accepted:true)
+  end
+
+  def accepted?
+    accepted
   end
 end
--- spec/features/accept_answer_spec.rb
+++ spec/features/accept_answer_spec.rb
@@ -5,25 +5,35 @@ feature 'Select Best Answer', %q{
   As author of the question
   I want to be able to pick the best answer
 }do
-
   describe 'Author of the question' do
-    scenario 'can see pick best answer button' do
+    scenario 'can see accept answer button' do
       author = create(:user)
       question = create(:question, user: author)
       answer = create(:answer, question: question)
       sign_in(author)
       visit question_path(question)
-      expect(page).to have_link 'pick-best-answer'
+      expect(page).to have_link 'accept as best'
     end
-    scenario 'pick best answer'
+
+    scenario 'pick best answer' do
+
+    end
+
     scenario 'can not pick best answer as best answer'
 #    scenario 'after pick best answer displayed first in the list'
 #    scenario 'There is only one best answer at the time for one question'
   end
 
-  # describe 'Non-author of the question' do
-  #   scenario ''
-  # end
+  describe 'Non-author of the question' do
+     scenario 'can not see accept answer button' do
+       author = create(:user)
+       question = create(:question, user: author)
+       answer = create(:answer, question: question)
+       sign_in(create(:user))
+       visit question_path(question)
+       expect(page).to_not have_link 'accept as best'
+     end
+   end
 
 #  scenario 'Author of the question re-pick best answer'
 
