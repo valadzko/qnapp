d5a24a85f425e19eef6a342663e5674d6b500bc0
valadzko <pavel.valadzko@gmail.com>
2016-11-10 16:53:11 +0300

    Refactoring

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -2,6 +2,7 @@
 
 class FileUploader < CarrierWave::Uploader::Base
 
+  delegate :identifier, to: :file
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -14,6 +15,10 @@ class FileUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def identifier
+    self.file.identifier
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
