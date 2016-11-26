593964fa5e471443e1f701c4df1ea84ccaf016e9
valadzko <pavel.valadzko@gmail.com>
2016-11-26 22:18:11 +0300

    Acceptance tests for voting for question

--- spec/features/user_vote_question_spec.rb
+++ spec/features/user_vote_question_spec.rb
@@ -6,16 +6,44 @@ feature 'User vote for question', %q{
   I want to upvote and down vote question
 } do
 
-  scenario 'Unauthenticated user can see the question rating' do
+  given!(:user){ create(:user) }
+  given!(:question){ create(:question, user: user) }
 
+  scenario 'Unauthenticated user can see the question rating' do
+    visit question_path(question)
+    expect(page).to have_content 'Rating: 0'
   end
 
-  scenario 'Authenticated user can vote up question'
-  scenario 'Author can not vote up and vote down for his own question'
-  scenario 'Authenticated user can down vote question'
-  scenario 'User can vote up again and it cancel question up vote'
-  scenario 'User can vote up again and it cancel question up vote'
-  scenario 'Non authenticated user can not up vote question'
-  scenario 'Non authenticated user can not down vote question'
+  scenario 'Author can not vote up and vote down for his own question' do
+    sign_in(user)
+    visit question_path(question)
+    expect(page).to_not have_link 'upvote'
+    expect(page).to_not have_link 'reset'
+    expect(page).to_not have_link 'downvote'
+  end
 
+  scenario 'Non authenticated user can not vote' do
+    visit question_path(question)
+    expect(page).to_not have_link 'upvote'
+    expect(page).to_not have_link 'reset'
+    expect(page).to_not have_link 'downvote'
+  end
+
+  scenario 'Authenticated user can vote up answer', js: true do
+    sign_in(create(:user))
+    visit question_path(question)
+    click_on 'upvote'
+    expect(page).to have_content 'Rating: 1'
+    click_on 'reset'
+    expect(page).to have_content 'Rating: 0'
+  end
+
+  scenario 'Authenticated user can vote down answer', js: true do
+    sign_in(create(:user))
+    visit question_path(question)
+    click_on 'downvote'
+    expect(page).to have_content 'Rating: -1'
+    click_on 'reset'
+    expect(page).to have_content 'Rating: 0'
+  end
 end
