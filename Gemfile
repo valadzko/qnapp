8142ab757a56629e7fc2f3115ba7cfa15b2de174
valadzko <pavel.valadzko@gmail.com>
2016-12-11 23:12:57 +0300

    Twitter omniauth

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ gem 'gon'
 gem 'responders'
 gem 'omniauth'
 gem 'omniauth-facebook'
+gem 'omniauth-twitter'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,8 +1,19 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  before_action :callback
+
   def facebook
-    @user = User.find_for_oauth(request.env['omniauth.oauth'])
+  end
+
+  def twitter
+  end
+
+  private
+
+  def callback
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+    end
   end
 end
