b8325f201357109162c16f3bf8ff55cf149a901e
valadzko <pavel.valadzko@gmail.com>
2016-12-22 16:53:00 +0300

    Fix for comments.build

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :user
 
+  validates :content, presence: true
+
   default_scope { order(:created_at) }
 end
